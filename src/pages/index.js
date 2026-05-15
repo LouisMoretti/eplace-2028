fbdefd979ed96ea4b8ac612f1a24e2660c463201
Louis Moretti <louis.moretti@epita.fr>
2026-05-15 12:51:01 +0200

    feat(auth): getToken

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 // FIXME: This is the entry point of the application, write your code here
 
 import { calculateLayout } from "./utils";
-// import { createLink, redirectToLoginPage } from "../utils/redirect";
+import { createLink, redirectToLoginPage } from "../utils/redirect";
 
 // Initialize the layout
 calculateLayout();
 
-// console.log(createLink());
+console.log(createLink());
 
-// redirectToLoginPage();
+redirectToLoginPage();
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,3 +4,27 @@
 // - refreshToken (refreshes the token using the refresh_token)
 // - authenticate (checks if the user is authenticated)
 // - authedAPIRequest (makes an authenticated request to the API)
+
+/**
+ * @param {string} code the authorization code received from the OIDC
+ * provider
+ * @returns {Promise<boolean>} true if the token was fetched, false otherwise
+ */
+export async function getToken(code) {
+    const formData = new FormData();
+
+    formData.append("grant_type", "authorization_code");
+    formData.append("code", code);
+    formData.append("redirect_uri", window.location.href);
+    formData.append("client_id", import.meta.env.VITE_CLIENT_ID);
+
+    const auth_url = import.meta.env.VITE_AUTH_URL;
+
+    return fetch(auth_url + "/token", { body: formData, method: "post" });
+}
+
+/**
+ * @param {string} refreshToken the refresh token (optional)
+ * @returns {Promise<boolean>} whether the token has been refreshed or not
+ */
+// export async function refreshToken(refreshToken) {}
